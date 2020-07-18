edd2ab5b3a4a0f4b7100351293236996be2c737f
Sergey Markov <msergei2008@gmail.com>
2020-07-18 23:58:23 +0500

    Initial Commit

---
+++ CurrencyViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CurrencyViewer
+//
+//  Created by Sergey Markov on 18.07.2020.
+//  Copyright © 2020 Sergey Markov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
